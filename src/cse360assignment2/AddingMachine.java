da24a3774f9838de68abfc500ace6e4cbcac900d
shadi <ssmoudi@asu.edu>
2020-10-03 12:51:12 -0700

    Initial Commit

---
+++ src/cse360assignment2/AddingMachine.java
@@ -0,0 +1,56 @@
+package cse360assignment2;
+
+/**
+ * Title: Version Control Assignment
+ * Description: Using GitHub to create a repository
+ * Class: CSE 360
+ * @author Shadi Smoudi
+ */
+
+public class AddingMachine {
+  private int total;
+  
+  /**
+   * 
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /**
+   * Description: 
+   * @return The result of the operation
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+   * Description: Adds integers
+   * @param value The integer being passed in
+   */
+  public void add (int value) {
+  }
+
+  /**
+   * Description: Subtracts integers
+   * @param value The value being passed in
+   */
+  public void subtract (int value) {
+  }
+
+  /**
+   * Description: Displays a string
+   * @return A string
+   */
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * Description: Clears the program inputs
+   */
+  public void clear() {
+  }
+}
+
